b612dcf2b1164182bf2634a0be32fe6200f83558
NguyenThanhNam127 <thanhnam120704@gmail.com>
2025-05-09 15:31:11 +0700

    init project

---
+++ BaiThucHanh1/app/src/main/java/com/example/baithuchanh1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.baithuchanh1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.*;
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtA, edtB, edtKQ;
+    Button btntong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //anh xa id cho cac bien giao dien
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btntong = findViewById(R.id.btntong);
+
+        //xu ly tuong tac voi nguoi dung
+        btntong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                int c = a + b;
+                edtKQ.setText(c + " ");
+            }
+        });
+    }
+}
---
+++ BaiThucHanh2/app/src/main/java/com/example/baithuchanh2/Subactivity.java
@@ -0,0 +1,33 @@
+package com.example.baithuchanh2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Subactivity extends AppCompatActivity {
+    Button btnOk;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_subactivity);
+        btnOk = findViewById(R.id.btnCall);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
