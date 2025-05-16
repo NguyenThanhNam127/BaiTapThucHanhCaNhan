3a0c1b9807c2d995dd1d4ecc82a5c6019e28ca12
NguyenThanhNam127 <thanhnam120704@gmail.com>
2025-05-16 14:12:21 +0700

    hello

---
+++ BaiThucHanh5/app/src/main/java/com/example/baithuchanh5/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.baithuchanh5;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_chuyendoi;
+    EditText edt_duonglich,edt_amlich;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        btn_chuyendoi = findViewById(R.id.btn_chuyendoi);
+        edt_amlich = findViewById(R.id.edt_amlich);
+        edt_duonglich= findViewById(R.id.edt_duonglich);
+
+        btn_chuyendoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String can ="",chi = "";
+                int namduong = Integer.parseInt(edt_duonglich.getText().toString());
+                switch (namduong%10){
+                    case 0: can= "Canh"; break;
+                    case 1: can= "Tân"; break;
+                    case 2: can= "Nhâm";break;
+                    case 3: can= "Quý";break;
+                    case 4: can= "Giáp";break;
+                    case 5: can= "Ất";break;
+                    case 6: can= "Bính";break;
+                    case 7: can= "Đinh";break;
+                    case 8: can= "Mậu";break;
+                    case 9: can= "Kỉ";break;
+
+                }
+
+                switch (namduong%12){
+                    case 0: chi="Thân"; break;
+                    case 1: chi="Dậu"; break;
+                    case 2: chi="Tuất"; break;
+                    case 3: chi="Hợi"; break;
+                    case 4: chi="Tí"; break;
+                    case 5: chi="Sửu"; break;
+                    case 6: chi="Dần"; break;
+                    case 7: chi="Mẹo"; break;
+                    case 8: chi="Thìn"; break;
+                    case 9: chi="Tỵ"; break;
+                    case 10: chi="Ngọ"; break;
+                    case 11: chi="Mùi"; break;
+                }
+
+                edt_amlich.setText(can + " " + chi);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
